c85a441494c550a149c42b3df550cf1b9a3234d3
Fabian Flores <ffflores1@outlook.com>
2018-04-29 16:11:21 -0400

    formatted survey form

--- app/data/surveyQuestions.js
+++ app/data/surveyQuestions.js
@@ -2,10 +2,34 @@ var uuid = require('uuid/v4');
 
 var surveyQuestions = [
   {
-    "question": "Your mind is always buzzing with unexplored ideas and plans"
+    "question": "You like being kind to other people."
   },
   {
-    "question": "Generally speaking, you rely more on your experience than your imagination"
+    "question": "You enjoy reading and learning."
+  },
+  {
+    "question": "You are able to handle stressful situations in a calm manner."
+  },
+  {
+    "question": "In general, you like to eat healthily."
+  },
+  {
+    "question": "Playing a game is fun only if you win."
+  },
+  {
+    "question": "You are an extroverted and enthusiastic person."
+  },
+  {
+    "question": "While financial well-being is important, this does not rule every aspect of your life."
+  },
+  {
+    "question": "You respect other people's points of view and like to have healthy discussions."
+  },
+  {
+    "question": "You do not get upset too easily."
+  },
+  {
+    "question": "You enjoy exercising."
   }];
 
 module.exports = surveyQuestions;
