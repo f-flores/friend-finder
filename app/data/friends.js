a8f20e76e444174f8af1c0d65ad9c491f9f6e7cd
Fabian Flores <ffflores1@outlook.com>
2018-04-29 16:47:25 -0400

    work on select menu in survey-layout.handlebars

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,17 +3,17 @@ var uuid = require('uuid/v4');
 var friendAnswers = [{
 	"name": "Fabian Flores",
 	"photo": "https://avatars0.githubusercontent.com/u/34374132",
-	"scores": [3,4]
+	"scores": [5,4,5,5,4,3,5,4,4,4]
 	},
 	{
 		"name": "Shaquille O'Neal",
 		"photo": "https://media2.giphy.com/media/k84osmLnefDcQ/giphy_s.gif",
-		"scores": [2, 5]
+		"scores": [3,4,5,1,4,2,4,2,1,2]
 	},
 	{
 		"name": "Danny DeVito",
 		"photo": "https://media3.giphy.com/media/ulWpAZPjLDYac/giphy_s.gif",
-		"scores": [1, 4]
+		"scores": [1,2,2,2,3,3,1,2,4,3]
 }];
 
 module.exports = friendAnswers;
